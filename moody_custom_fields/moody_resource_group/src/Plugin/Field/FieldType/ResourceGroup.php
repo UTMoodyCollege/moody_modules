09ed551273d63ef540b68202a898d5de2727b8d3
Marsello <michaelmarsello@gmail.com>
2021-10-04 12:04:42 -0500

    Add resource group module

---
+++ moody_custom_fields/moody_resource_group/src/Plugin/Field/FieldType/ResourceGroup.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\moody_resource_group\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'resource_group' field type.
+ *
+ * @FieldType(
+ *   id = "resource_group",
+ *   label = @Translation("Resource group"),
+ *   description = @Translation("List of links with stylized headline"),
+ *   category = @Translation("Moody"),
+ *   default_widget = "resource_group_widget",
+ *   default_formatter = "resource_group_formatter"
+ * )
+ */
+class ResourceGroup extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    // Prevent early t() calls by using the TranslatableMarkup.
+    $properties['headline'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Headline value'))
+      ->setRequired(FALSE);
+    $properties['style'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Style value'))
+      ->setRequired(FALSE);
+    $properties['links'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Links'))
+      ->setRequired(FALSE);
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = [
+      'columns' => [
+        'headline' => [
+          'type' => 'varchar',
+          'length' => 512,
+          'binary' => FALSE,
+        ],
+        'style' => [
+          'type' => 'varchar',
+          'length' => 512,
+          'binary' => FALSE,
+        ],
+        'links' => [
+          'type' => 'blob',
+          'size' => 'normal',
+        ],
+      ],
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
+    $random = new Random();
+    $values['value'] = $random->word(mt_rand(1, $field_definition->getSetting('max_length')));
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $headline = $this->get('headline')->getValue();
+    $links = $this->get('links')->getValue();
+    return $headline === NULL || $headline === '' && empty($links);
+  }
+
+}
