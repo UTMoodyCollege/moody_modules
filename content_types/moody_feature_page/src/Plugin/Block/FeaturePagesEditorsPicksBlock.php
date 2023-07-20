87707885a29a3d6ad99d49db84ed63d891c53133
Tyler Fahey <twfahey1@gmail.com>
2023-07-20 15:08:15 -0400

    Scaffold a Feature Page Editors Picks block

---
+++ content_types/moody_feature_page/src/Plugin/Block/FeaturePagesEditorsPicksBlock.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\moody_feature_page\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a feature pages - editors picks block.
+ *
+ * @Block(
+ *   id = "moody_feature_page_feature_pages_editors_picks",
+ *   admin_label = @Translation("Feature Pages - Editors Picks"),
+ *   category = @Translation("Custom"),
+ * )
+ */
+final class FeaturePagesEditorsPicksBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Constructs the plugin instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array {
+    return [
+      'example' => $this->t('Hello world!'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state): array {
+    $form['example'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Example'),
+      '#default_value' => $this->configuration['example'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    $this->configuration['example'] = $form_state->getValue('example');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build['content'] = [
+      '#markup' => $this->t('It works!'),
+    ];
+    return $build;
+  }
+
+}
