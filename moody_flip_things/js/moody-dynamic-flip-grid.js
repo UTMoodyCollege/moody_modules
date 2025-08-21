cb029e0df32c81bd2d7df79d5f0f9e31dba34408
twfahey1 <twfahey1@gmail.com>
2025-08-21 09:23:11 -0500

    Revise to use proper modern once() implementation

--- moody_flip_things/js/moody-dynamic-flip-grid.js
+++ moody_flip_things/js/moody-dynamic-flip-grid.js
@@ -1,15 +1,20 @@
 /**
- * JavaScript for Moody Dynamic Flip Grid behavior.
+ * JavaScript for Moody Dynamic Flip Grid behavior (Drupal 10+ style).
  */
 
-(function ($, Drupal, drupalSettings) {
-    Drupal.behaviors.moody_dynamic_flip_grid = {
-      attach: function (context, settings) {
-        $(".moody-dynamic-flip-grid-item").once('dynamic-flip-grid').each(function() {
-          $(this).flip({
-            trigger: "hover",
-          });
-        });
-      },
-    };
-  })(jQuery, Drupal, drupalSettings);
+
+(function (Drupal) {
+  Drupal.behaviors.moody_dynamic_flip_grid = {
+    attach(context) {
+      document.querySelectorAll('.moody-dynamic-flip-grid-item').forEach((el) => {
+        if (once('dynamic-flip-grid', el, context).length) {
+          if (typeof jQuery(el).flip === 'function') {
+            jQuery(el).flip({
+              trigger: 'hover',
+            });
+          }
+        }
+      });
+    },
+  };
+})(Drupal);
