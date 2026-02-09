afb357d8bb5e01c4617e026450665bf16b0c6ff7
twfahey1 <twfahey1@gmail.com>
2026-02-09 12:13:20 -0600

    Update MoodyFlexGridElement to extend FormElementBase for Drupal 11

--- moody_custom_fields/moody_flex_grid/src/Element/MoodyFlexGridElement.php
+++ moody_custom_fields/moody_flex_grid/src/Element/MoodyFlexGridElement.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\moody_flex_grid\Element;
 
-use Drupal\Core\Render\Element\FormElement;
+use Drupal\Core\Render\Element\FormElementBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -10,7 +10,7 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @FormElement("moody_flex_grid")
  */
-class MoodyFlexGridElement extends FormElement {
+class MoodyFlexGridElement extends FormElementBase {
 
   /**
    * {@inheritdoc}
